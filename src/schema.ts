6c49f3d22aa9aa373f1f4f8329154eae4cac917e
fahimahammed <fahimahammed.cse@gmail.com>
2023-05-25 22:53:27 +0600

    schema added > 55>1.45

--- src/schema.ts
+++ src/schema.ts
@@ -7,4 +7,22 @@ export const typeDefs = `#graphql
   type Query {
     books: [Book]
   }
+
+  type Post {
+    id: ID!
+    title: String!
+    content: String!
+    createdAt: String!
+    published: Boolean!
+    user: User!
+  }
+
+  type {
+    User
+    [Post]
+  }
+
+  type Profile {
+    User
+  }
 `;
